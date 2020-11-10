20e19e956b589af3fcaf3c81ad8fa7e729ba335a
YuHeng-Juan <jimmy7511685@gmail.com>
2020-11-10 19:26:30 +0800

    fuck you!

--- request.py
+++ request.py
@@ -8,6 +8,9 @@ import cgi
 import os
 import time
 app = Flask(__name__)
+
+#fuck you man. I wanna kick your ass shit man.
+
 #student_ID=""
 app.config['SESSION_PERMANENT'] = True
 app.config['SESSION_USE_SIGNER'] = False  
