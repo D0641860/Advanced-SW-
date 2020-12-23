9be82eab668a3bc374af6a0a8a821292f4fb3f39
D0641860 <zs1452ax36f169@gmail.com>
2020-12-23 13:36:37 +0800

    1223

--- test/test.py
+++ test/test.py
@@ -22,8 +22,8 @@ options.add_argument("--no-sandbox") #This make Chromium reachable
 options.add_argument("--no-default-browser-check") #Overrides default choices
 options.add_argument("--no-first-run")
 options.add_argument("--disable-default-apps") 
-#driver = webdriver.Chrome('/home/travis/virtualenv/python3.7.9/bin/chromedriver',chrome_options=options)
-driver = webdriver.Chrome("/path/to/chrome",chrome_options=options)
+driver = webdriver.Chrome('/home/travis/virtualenv/python3.7.9/bin/chromedriver',chrome_options=options)
+#driver = webdriver.Chrome("/path/to/chrome",chrome_options=options)
 class test(unittest.TestCase):
     
     #def setUp(self): #每一次執行測試方法"前"會執行
