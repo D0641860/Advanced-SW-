8864bb31d424ee8c62717de40224681d187e593c
D0641860 <zs1452ax36f169@gmail.com>
2020-12-23 14:29:08 +0800

    1223

--- test/test.py
+++ test/test.py
@@ -15,19 +15,19 @@ cursor=db.cursor()
 test_username = ["admin","test1","test2","test3"]
 test_password = ["admin2016","1test2020","2test2020","3test2020"] 
 
-#options = webdriver.ChromeOptions()
+options = webdriver.ChromeOptions()
 #options.binary_location = '/usr/bin/chromium-browser'
 #All the arguments added for chromium to work on selenium
-#options.add_argument("--no-sandbox") #This make Chromium reachable
-#options.add_argument("--no-default-browser-check") #Overrides default choices
-#options.add_argument("--no-first-run")
-#options.add_argument("--disable-default-apps") 
-#driver = webdriver.Chrome('/home/travis/virtualenv/python3.7.9/bin/chromedriver',chrome_options=options)
-driver = webdriver.Chrome("./chromedriver")
+options.add_argument("--no-sandbox") #This make Chromium reachable
+options.add_argument("--no-default-browser-check") #Overrides default choices
+options.add_argument("--no-first-run")
+options.add_argument("--disable-default-apps") 
+driver = webdriver.Chrome('/home/travis/virtualenv/python3.7.9/bin/chromedriver',chrome_options=options)
+#driver = webdriver.Chrome("/path/to/chrome",chrome_options=options)
 class test(unittest.TestCase):
     
     #def setUp(self): #每一次執行測試方法"前"會執行
-    #   self.browser = webdriver.Chrome('./chromedriver')
+    #    self.browser = webdriver.Chrome('./chromedriver')
     #   self.f = open('test_record.txt', 'w')
     
     def test_register_login(self):
@@ -63,7 +63,7 @@ class test(unittest.TestCase):
             cursor.execute(select)
             data=cursor.fetchone()
             print(data)
-            #self.assertIsNotNone(data,msg="Failed!")
+            self.assertIsNotNone(data,msg="Failed!")
             print("{} Successful!".format(test_username[i]))
             #f.write("{} Successful!\n".format(test_username[i]))
             i += 1
